429f1019df91ddd250bcc0f0c2860b32794f30b9
= <=>
2021-05-20 21:38:04 +0200

    [UPDATE] first question

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return the number of subscribers from a subreddit"""
-    path = "https://www.reddit.com/r/{" + subreddit + "}/about.json"
+    path = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(path, allow_redirects=False, headers=headers)
     if response.status_code in (302, 404):
